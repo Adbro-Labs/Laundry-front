76f64549cdaea07e1be4ca5abb8e987a4cf80cf2
decoy <mh40789@gmail.com>
2024-10-26 12:26:26 +0530

    removed unwanted modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RefDirective } from './shared/directives/ref.directive';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RequestInterceptor } from './shared/interceptor/request.interceptor';
-// AoT requires an exported function for factories
-export const createTranslateLoader = (http: HttpClient) => {
-    /* for development
-    return new TranslateHttpLoader(
-        http,
-        '/start-javascript/sb-admin-material/master/dist/assets/i18n/',
-        '.json'
-    );*/
-    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-};
+
 
 @NgModule({
     declarations: [AppComponent, RefDirective],
@@ -33,14 +22,7 @@ export const createTranslateLoader = (http: HttpClient) => {
         OverlayModule,
         HttpClientModule,
         FormsModule,
-        ReactiveFormsModule,
-        TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: createTranslateLoader,
-                deps: [HttpClient]
-            }
-        })
+        ReactiveFormsModule
     ],
     providers: [{ provide: HTTP_INTERCEPTORS, useClass: RequestInterceptor, multi: true }],
     bootstrap: [AppComponent]
