336cd3831e4ba143353e80528a04753ff9c2ea57
Mubashir <mubashirmuchi75@gmail.com>
2025-05-17 14:36:59 +0530

    permission modal

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationPermissionDialogComponent } from './notification-permission-dialog/notification-permission-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-layout',
@@ -6,7 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
-  constructor() {}
+  constructor(private dialog: MatDialog) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.requestNotificationPermission();
+  }
+
+  isDialogOpen = false;
+
+  private async requestNotificationPermission(): Promise<void> {
+    try {
+      const permission = await Notification.requestPermission();
+
+      if (permission === 'granted') {
+        alert('Notifications allowed');
+      } else if (!this.isDialogOpen) {
+        this.openNotificationDialog(permission);
+      }
+    } catch (error) {
+      console.error('Error requesting notification permission:', error);
+    }
+  }
+
+  openNotificationDialog(permission: NotificationPermission): void {
+    this.isDialogOpen = true;
+
+    const dialogRef = this.dialog.open(NotificationPermissionDialogComponent, {
+      data: { permission },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      this.isDialogOpen = false;
+
+      if (result === 'retry') {
+        this.requestNotificationPermission();
+      }
+    });
+  }
 }
---
+++ src/app/layout/notification-permission-dialog/notification-permission-dialog.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NotificationPermissionDialogComponent } from './notification-permission-dialog.component';
+
+describe('NotificationPermissionDialogComponent', () => {
+  let component: NotificationPermissionDialogComponent;
+  let fixture: ComponentFixture<NotificationPermissionDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NotificationPermissionDialogComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NotificationPermissionDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
